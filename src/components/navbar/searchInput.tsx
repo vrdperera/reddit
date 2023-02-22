7804b6ba00f80fb6a000ce4c21a687ad2ea732bd
Vrd Perera <vrdperera@gmail.com>
2023-02-22 22:07:41 +0530

    complete searchInput

--- src/components/navbar/searchInput.tsx
+++ src/components/navbar/searchInput.tsx
@@ -7,12 +7,29 @@ interface IsearcInputProps {
 
 function SearchInput({ user }: IsearcInputProps) {
   return (
-    <Flex>
+    <Flex flexGrow={1} align="center" mr={2}>
       <InputGroup>
         <InputLeftElement pointerEvents="none">
-          <SearchIcon color="gray.300" />
+          <SearchIcon color="gray.400" mb={1} />
         </InputLeftElement>
-        <Input type="tel" placeholder="Phone number" />
+        <Input
+          type="tel"
+          placeholder="Search Reddit"
+          fontSize="10pt"
+          _placeholder={{ color: 'gray.500' }}
+          _hover={{
+            bg: 'white',
+            borderColor: 'blue.500',
+            border: '1px solid ',
+          }}
+          _focus={{
+            outline: 'none',
+            borderColor: 'blue.500',
+            border: '1px solid',
+          }}
+          h="34px"
+          bg="gray.50"
+        />
       </InputGroup>
     </Flex>
   );
