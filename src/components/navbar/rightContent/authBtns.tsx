05c1d875401448f7f5051d6003e2c6c5958f9794
Vrd Perera <vrdperera@gmail.com>
2023-02-27 15:57:37 +0530

    complete auth buttons

---
+++ src/components/navbar/rightContent/authBtns.tsx
@@ -0,0 +1,32 @@
+import { Button } from '@chakra-ui/react';
+import { useSetRecoilState } from 'recoil';
+import { AuthModalState, authModalState } from '@/atoms/authModalAtom';
+
+function AuthBtns() {
+  const setAuthModalState = useSetRecoilState<AuthModalState>(authModalState);
+  return (
+    <>
+      <Button
+        variant="outline"
+        height="28px"
+        display={{ base: 'none', sm: 'unset' }}
+        width={{ base: '70px', md: '110px' }}
+        mr={2}
+        onClick={() => setAuthModalState({ open: true, view: 'login' })}
+      >
+        Log In
+      </Button>
+      <Button
+        height="28px"
+        display={{ base: 'none', sm: 'unset' }}
+        width={{ base: '70px', md: '110px' }}
+        mr={2}
+        onClick={() => setAuthModalState({ open: true, view: 'login' })}
+      >
+        Sign Up
+      </Button>
+    </>
+  );
+}
+
+export default AuthBtns;
