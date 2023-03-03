2e53ae7c3e33cfb363370485fdee44a8eba52b34
Vrd Perera <vrdperera@gmail.com>
2023-03-03 11:08:13 +0530

    add errorMessage component

---
+++ src/components/modal/auth/errorMessage.tsx
@@ -0,0 +1,17 @@
+import { Text } from '@chakra-ui/react';
+
+interface IErrorMessageProps {
+  error: string | undefined;
+}
+
+function ErrorMessage({ error }: IErrorMessageProps) {
+  if (!error) return null;
+
+  return (
+    <Text textAlign="center" fontSize="10pt" color="red" mt={2}>
+      {error}
+    </Text>
+  );
+}
+
+export default ErrorMessage;
