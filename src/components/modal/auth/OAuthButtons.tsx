841dee7ad99a51cbb9928a1a771a4693555d4a4f
Vrd Perera <vrdperera@gmail.com>
2023-03-03 13:03:24 +0530

    add firebase google authentication

--- src/components/modal/auth/OAuthButtons.tsx
+++ src/components/modal/auth/OAuthButtons.tsx
@@ -1,9 +1,30 @@
+import { auth } from '@/firebase/clientApp';
+import { FIREBASE_ERRORS } from '@/firebase/erros';
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import ErrorMessage from './errorMessage';
+
 import { Button, Image } from '@chakra-ui/react';
 
 function OAuthButtons() {
+  const [signInWithGoogle, user, loading, authError] =
+    useSignInWithGoogle(auth);
+
   return (
     <div className="mb-4 flex w-full flex-col gap-2">
-      <Button variant="oauth">
+      <ErrorMessage
+        error={
+          authError?.message &&
+          FIREBASE_ERRORS[authError.message as keyof typeof FIREBASE_ERRORS]
+        }
+      />
+      <Button
+        variant="oauth"
+        isLoading={loading}
+        onClick={() => {
+          signInWithGoogle();
+        }}
+        mt={2}
+      >
         <Image src="/images/googlelogo.png " alt="Google Logo" w={5} mr={4} />
         Continue with Google
       </Button>
