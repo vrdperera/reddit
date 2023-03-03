2e3ef33db257db54a9f83e75b77be04ae53b40ee
Vrd Perera <vrdperera@gmail.com>
2023-03-03 13:25:13 +0530

    add firebase login functionality

--- src/components/modal/auth/login.tsx
+++ src/components/modal/auth/login.tsx
@@ -1,8 +1,14 @@
-import { authModalState, AuthModalState } from '@/atoms/authModalAtom';
-import { Button, Flex, Input, Text } from '@chakra-ui/react';
 import { useState } from 'react';
 import { useSetRecoilState } from 'recoil';
+import { authModalState, AuthModalState } from '@/atoms/authModalAtom';
+
+import { auth } from '@/firebase/clientApp';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+
+import { Button, Flex, Input, Text } from '@chakra-ui/react';
 import InputField from './inputField';
+import ErrorMessage from './errorMessage';
+import { FIREBASE_ERRORS } from '@/firebase/erros';
 
 interface ILoginFromValues {
   email: string;
@@ -11,17 +17,21 @@ interface ILoginFromValues {
 
 function Login() {
   const setAuthModalState = useSetRecoilState<AuthModalState>(authModalState);
-
   const [loginFromValues, setLoginFromValues] = useState<ILoginFromValues>({
     email: '',
     password: '',
   });
+  const [signInWithEmailAndPassword, user, loading, authError] =
+    useSignInWithEmailAndPassword(auth);
 
-  // With Firebase
+  // console.log(user);
+
+  //handle form submission with Firebase
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    // handle form submission
+    signInWithEmailAndPassword(loginFromValues.email, loginFromValues.password);
   };
+
   const handleChange = ({ target }: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = target;
 
@@ -94,9 +104,18 @@ function Login() {
         }}
         bg="gray.50"
       /> */}
-      <Button w="100%" mt={4} mb={2} h="36px" type="submit">
+
+      <ErrorMessage
+        error={
+          authError?.message &&
+          FIREBASE_ERRORS[authError.message as keyof typeof FIREBASE_ERRORS]
+        }
+      />
+
+      <Button w="100%" mt={4} mb={2} h="36px" type="submit" isLoading={loading}>
         Login
       </Button>
+
       <Flex fontSize="9pt" justify="center" gap={2}>
         <Text className="cursor-pointer">New to Reddit?</Text>
         <Text
