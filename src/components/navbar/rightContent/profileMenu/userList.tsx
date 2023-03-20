41a13fd86169e12610c829b1ad8135d5f28ff4b9
Vrd Perera <vrdperera@gmail.com>
2023-03-20 14:05:14 +0530

    add reset recoil state functionality

--- src/components/navbar/rightContent/profileMenu/userList.tsx
+++ src/components/navbar/rightContent/profileMenu/userList.tsx
@@ -5,37 +5,28 @@ import { CgProfile } from 'react-icons/cg';
 import { MdOutlineLogin } from 'react-icons/md';
 import { useResetRecoilState } from 'recoil';
 import { auth } from '@/firebase/clientApp';
-// import { communityState } from '../../../../atoms/communitiesAtom';
+import { communityState } from '@/atoms/communitiesAtom';
 
 type UserListProps = {};
 
 function UserList() {
-  //   const resetCommunityState = useResetRecoilState(communityState);
+  const resetCommunityState = useResetRecoilState(communityState);
 
   const logout = async () => {
     await signOut(auth);
-    // resetCommunityState();
+    resetCommunityState();
   };
 
   return (
     <>
-      <MenuItem
-        fontSize="10pt"
-        fontWeight={700}
-        _hover={{ bg: 'blue.500', color: 'white' }}
-      >
+      <MenuItem fontSize="10pt" fontWeight={700} _hover={{ bg: 'blue.500', color: 'white' }}>
         <Flex alignItems="center">
           <Icon fontSize={20} mr={2} as={CgProfile} />
           Profile
         </Flex>
       </MenuItem>
       <MenuDivider />
-      <MenuItem
-        fontSize="10pt"
-        fontWeight={700}
-        _hover={{ bg: 'blue.500', color: 'white' }}
-        onClick={logout}
-      >
+      <MenuItem fontSize="10pt" fontWeight={700} _hover={{ bg: 'blue.500', color: 'white' }} onClick={logout}>
         <Flex alignItems="center">
           <Icon fontSize={20} mr={2} as={MdOutlineLogin} />
           Log Out
